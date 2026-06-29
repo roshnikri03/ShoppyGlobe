16f7778c208b7cd5ad62e98c9cda8c4788ca721c
roshnikri03 <roshnikumari90403@gmail.com>
2026-06-29 18:40:17 +0530

    feat: add SuspenseFallback component for loading state indication

---
+++ src/components/SuspenseFallback.jsx
@@ -0,0 +1,9 @@
+const SuspenseFallback = () => {
+  return (
+    <div className="spinner-container">
+      <div className="spinner"></div>
+    </div>
+  );
+};
+
+export default SuspenseFallback;
